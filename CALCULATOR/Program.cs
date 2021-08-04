f2a2db60a745f756338dd3b3a6fa414743e078ef
Ernesto Ruiz <Ernesto Ruiz@MIGUELRUIZ>
2021-08-04 09:29:08 -0500

    Agregar archivos de proyecto.

---
+++ CALCULATOR/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using CalculatorLibrary;
+
+namespace CalculatorProgram
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+           
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            Calculator calculator = new Calculator();
+            while (!endApp)
+            {
+                // Se declaran variables.
+                string numeroInput1 = "";
+                string numeroInput2 = "";
+                double resultado = 0;
+
+               
+                Console.Write("Digite el primer número y presione Enter: ");
+                numeroInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numeroInput1, out cleanNum1))
+                {
+                    Console.Write("Esta no es una entrada válida. Ingrese un valor entero: ");
+                    numeroInput1 = Console.ReadLine();
+                }
+
+              
+                Console.Write("Digite el segundo número y presione Enter: ");
+                numeroInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numeroInput2, out cleanNum2))
+                {
+                    Console.Write("Esta no es una entrada válida. Ingrese un valor entero: ");
+                    numeroInput2 = Console.ReadLine();
+                }
+
+              
+                Console.WriteLine("Seleccione la operación que desea realizar:");
+                Console.WriteLine("\ts - Suma");
+                Console.WriteLine("\tr - Resta");
+                Console.WriteLine("\tm - Multiplicación");
+                Console.WriteLine("\td - Divición");
+                Console.Write("Tú opción es: ");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    resultado = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(resultado))
+                    {
+                        Console.WriteLine("Esta operación resultará en un error matemático.\n");
+                    }
+                    else Console.WriteLine("Tú resultado es: {0:0.##}\n", resultado);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! Se produjo una excepción al intentar hacer los cálculos.\n - Detalles: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+               
+                Console.Write("Presione 'n' y Enter para cerrar la aplicación, o presione cualquier otra tecla y Enter para continuar: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); 
+            }
+            calculator.Finish();
+            return;
+        }
+    }
+}
---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{ 
+    public class Calculator
+    {
+
+        JsonWriter writer; 
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        public double DoOperation(double numero1, double numero2, string op)
+        {
+            double resultado = double.NaN; 
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(numero1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(numero2);
+            writer.WritePropertyName("Operation");
+            
+            switch (op)
+            {
+                case "s":
+                    resultado = numero1 + numero2;
+                    writer.WriteValue("Suma");
+                    break;
+                case "r":
+                    resultado = numero1 - numero2;
+                    writer.WriteValue("Resta");
+                    break;
+                case "m":
+                    resultado = numero1 * numero2;
+                    writer.WriteValue("Multiplicación");
+                    break;
+                case "d":
+                    
+                    if (numero2 != 0)
+                    {
+                        resultado = numero1 / numero2;
+                        writer.WriteValue("Divición");
+                    }
+                    break;
+                
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Resultado");
+            writer.WriteValue(resultado);
+            writer.WriteEndObject();
+
+            return resultado;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
